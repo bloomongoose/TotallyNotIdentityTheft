b0f6f9777e9a99db892eabcaaa449e36735cf916
radeen <radeen013@gmail.com>
2021-09-27 13:36:06 -0400

    first commit

---
+++ Day1Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day1Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there! We'll be needing some info from you today.");
+
+            Console.WriteLine("Please enter your name.");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Great! Please enter your age.");
+            float age = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wonderful! Now enter your hometown.");
+            string homeTown = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name}. Your age is {age}. Your hometown is {homeTown}.");
+        }
+    }
+}
